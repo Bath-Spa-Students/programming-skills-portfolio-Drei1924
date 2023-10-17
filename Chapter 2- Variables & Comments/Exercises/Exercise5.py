fc409775f4b29d606673e4d98635c7e465980a20
Drei1924 <144208206+Drei1924@users.noreply.github.com>
2023-10-17 09:31:49 +0400

    Update Exercise5.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise5.py
@@ -0,0 +1,7 @@
+usb_stick_cost = 6
+total_budget = 50
+
+remaining_money = total_budget % usb_stick_cost
+
+print (f'The Total of USB sticks is {50/6}.')
+print ("The Remaining money is:", remaining_money, "pounds")
