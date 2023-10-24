5ef50ef86d953f1fa22440e5678410c889bca965
Drei1924 <144208206+Drei1924@users.noreply.github.com>
2023-10-24 12:58:07 +0400

    Create Exercise7.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise7.py
@@ -0,0 +1,23 @@
+places_to_visit = ["Tokyo, " "New York, "  "Canada, "  "Mexico"]
+
+print("Original order:", places_to_visit)
+
+print("\nAlphabetical order:", sorted(places_to_visit))
+
+print("\noriginal order:", places_to_visit)
+
+print("\nReverse alphabetical order:", sorted(places_to_visit, reverse=True))
+
+print("\nOriginal order:", places_to_visit)
+
+places_to_visit.reverse()
+print("\nReversed order:", places_to_visit)
+
+places_to_visit.reverse()
+print("\nOriginal order:", places_to_visit)
+
+places_to_visit.sort()
+print("\nAlphabetical order:", places_to_visit)
+
+places_to_visit.sort(reverse=True)
+print("\nReverse alphabetical order:", places_to_visit)
