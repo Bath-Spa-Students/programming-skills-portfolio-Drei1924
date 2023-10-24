914e6fc332dc0a41624182ae0cbad96d74d42d92
Drei1924 <144208206+Drei1924@users.noreply.github.com>
2023-10-24 12:57:26 +0400

    Create Exercise4.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise4.py
@@ -0,0 +1,6 @@
+invitation_list = ["Kristel Mae", "Mark Jimsell", "Jao Deleon"]
+
+
+for person in invitation_list:
+    print(f"Hello {person},\n\tYou are Invited to a dinner. It would be nice to have you.\nSincerely, Jyosef Drei Lozanes")
+    print("\n")
