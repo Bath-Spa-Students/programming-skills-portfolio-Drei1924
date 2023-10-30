a0313d82b6c56faa6bf098d006bd56b75f49fa05
Drei1924 <144208206+Drei1924@users.noreply.github.com>
2023-10-30 14:47:42 +0400

    Create Exercise1.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise1.py
@@ -0,0 +1,10 @@
+alien_color = 'green'
+
+if alien_color == 'green':
+
+    print("You Just Earned 5 Points!")
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("You Just Earned 5 Points!")
