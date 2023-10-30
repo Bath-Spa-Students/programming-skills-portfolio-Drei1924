b68e60a39e4e0018c4e9f922c5f9eb235e742d41
Drei1924 <144208206+Drei1924@users.noreply.github.com>
2023-10-30 14:48:33 +0400

    Create Exercise3.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise3.py
@@ -0,0 +1,13 @@
+alien_color = input("Enter a color of your choice: ")
+
+if alien_color == "green":
+    print("You just earned 5 points")
+
+elif alien_color == "yellow":
+    print("You just earned 10 points")
+
+elif alien_color == "red":
+    print("You just earned 15 points")
+
+else:
+    print("\nInvalid Output")
