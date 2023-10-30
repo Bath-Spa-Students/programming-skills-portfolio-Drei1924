30236900fe1ced077fec5446ee63ef4f1a87fdf4
Drei1924 <144208206+Drei1924@users.noreply.github.com>
2023-10-30 14:51:52 +0400

    Create Exercise5.py

---
+++ Chapter 4- Control Flow/Exercises/Exercise5.py
@@ -0,0 +1,14 @@
+favorite_fruits = input("What fruit do you like?: ")
+
+if favorite_fruits == "banana":
+    print("You really love banana!")
+
+elif favorite_fruits == "apple":
+    print("Your really love apple!")
+
+elif favorite_fruits == "orange":
+    print("You really love orange!")
+
+else:
+    print("\nInvalid Fruit!")
+
