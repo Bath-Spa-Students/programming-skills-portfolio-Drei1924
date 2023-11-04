12c5d31c57c9fe550f29fbfa0678e637891dd430
Drei1924 <144208206+Drei1924@users.noreply.github.com>
2023-11-04 15:20:41 +0400

    Create Exercise3.py

---
+++ Chapter 7- Functions/Exercises/Exercise3.py
@@ -0,0 +1,6 @@
+def make_shirt(size, message):
+    print(f"You have ordered a {size} shirt with the message: '{message}'.")
+
+make_shirt("Large", "Nevermore")
+
+make_shirt(size="Extra Large", message="Jett Diff")
