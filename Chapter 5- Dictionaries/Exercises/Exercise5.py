3845b070dd48a92713da12c2d0a2e087cc7008f2
Drei1924 <144208206+Drei1924@users.noreply.github.com>
2023-11-04 14:26:35 +0400

    Create Exercise5.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise5.py
@@ -0,0 +1,16 @@
+pets = [
+    {
+        'kind': 'Dog',
+        'owner': 'Sasha'},
+    {
+        'kind': 'Cat',
+        'owner': 'Mae'},
+    {
+        'kind': 'Lion',
+        'owner': 'Drei' }, ]
+       
+for pet in pets:
+    print(f"Kind of Animal: {pet['kind']}")
+    print(f"Owner's Name: {pet['owner']}\n")
+
+
