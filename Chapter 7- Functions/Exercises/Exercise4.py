471d1378b8ef07d8bfcbe99a00aa1aa44ea8f672
Drei1924 <144208206+Drei1924@users.noreply.github.com>
2023-11-04 15:20:45 +0400

    Create Exercise4.py

---
+++ Chapter 7- Functions/Exercises/Exercise4.py
@@ -0,0 +1,8 @@
+def make_shirt(size="Large", message="Logitech "):
+    print(f"You have ordered a {size} shirt with the message: '{message}'.")
+
+make_shirt()
+
+make_shirt("medium")
+
+make_shirt("Extra Large", "Steel Series!")
