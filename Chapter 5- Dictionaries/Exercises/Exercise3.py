366a02f993709c7122c0531fa548991f120a9cf3
Drei1924 <144208206+Drei1924@users.noreply.github.com>
2023-11-04 14:26:27 +0400

    Create Exercise3.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise3.py
@@ -0,0 +1,23 @@
+glossary = {
+    'input()function': 'This allows the user to input information into program .',
+    
+    'variables': 'A named container that stores data.',
+
+    'loop': 'A control structure that repeats a block of code multiple times.',
+
+    'Float': 'A numerical value with a decimal component.',
+
+    'string': 'Composed of a collection of characters',}
+
+glossary['dictionary'] = 'A collection of key-value pairs.'
+
+glossary['list'] = 'An ordered collection of items that can be of different types.'
+
+glossary['integer'] = 'Use for whole number, positive or negative or zero without decimals.'
+
+glossary['elif'] = 'Conditional statements to introduce an "else if" .'
+
+glossary['import'] = 'A keyword used to bring external modules or packages into your code.'
+
+for term, definition in glossary.items():
+    print(f"{term}: {definition}\n")
