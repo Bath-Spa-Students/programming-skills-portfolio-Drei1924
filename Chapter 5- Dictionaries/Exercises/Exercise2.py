3173a05443d0b20c012d9439462580e8420f600a
Drei1924 <144208206+Drei1924@users.noreply.github.com>
2023-11-04 14:26:22 +0400

    Create Exercise2.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise2.py
@@ -0,0 +1,13 @@
+glossary = {
+    'input()function': 'This allows the user to input information into program .',
+    
+    'variables': 'A named container that stores data.',
+
+    'loop': 'A control structure that repeats a block of code multiple times.',
+
+    'Float': 'A numerical value with a decimal component.',
+
+    'string': 'Composed of a collection of characters',}
+
+for word, meaning in glossary.items():
+    print(f"{word}: {meaning}\n")
